e20c3653738949d1c6ba8bf006e4826903979398
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-12-11 11:20:08 +0100

    use conventional order in cmake commands

--- scripts/build.example.sh
+++ scripts/build.example.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 cd build
-cmake .. "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=${WORKSPACE}\sysconfig\cmake\physycom_toolchain.cmake" "-DCMAKE_BUILD_TYPE=Release"
+cmake "-DVCPKG_CHAINLOAD_TOOLCHAIN_FILE=${WORKSPACE}\sysconfig\cmake\physycom_toolchain.cmake" "-DCMAKE_BUILD_TYPE=Release" ..
 cmake --build . 
 cd ..
