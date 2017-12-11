7b867ba22905e5127d0b45f1bc93b84ba49c4219
Stefano Sinigardi <stesinigardi@hotmail.com>
2017-12-11 11:08:36 +0100

    properly set default build type

--- cmake/physycom_toolchain.cmake
+++ cmake/physycom_toolchain.cmake
@@ -68,8 +68,13 @@ endif()
 
 
 # make sure that the default is a Release
-if(NOT CMAKE_BUILD_TYPE)
-  set(CMAKE_BUILD_TYPE "Release")
+set(default_build_type "Release")
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
+  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
+      STRING "Choose the type of build." FORCE)
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+    "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
 
 
