84cfebd1e1eb3f62d0de4eecc9a2e1ca12f7aa65
Stefano Sinigardi <stesinigardi@hotmail.com>
2018-02-01 19:02:40 +0100

    introduce physycom vcpkg triplet

--- scripts/build.example.ps1
+++ scripts/build.example.ps1
@@ -4,11 +4,7 @@ Remove-Item build -Force -Recurse -ErrorAction SilentlyContinue
 New-Item -Path .\build -ItemType directory -Force
 Set-Location build
 
-#dynamically linked
-#cmake -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release" ..
-
-#statically linked
-cmake -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:WORKSPACE\vcpkg\scripts\buildsystems\vcpkg.cmake" "-DVCPKG_TARGET_TRIPLET=x86-windows-static" "-DCMAKE_BUILD_TYPE=Release" ..
+cmake -G "Visual Studio 15" "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" "-DCMAKE_BUILD_TYPE=Release" ..
 
 cmake --build . --config Release
 
